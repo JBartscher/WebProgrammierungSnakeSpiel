8e1e4e3679377a572c03a441438d976b744720ac
jasper <jasper.bartscher@gmail.com>
2020-12-03 15:23:06 +0100

    redirect after Form Submit

--- SnakeGame/src/main.js
+++ SnakeGame/src/main.js
@@ -5,6 +5,7 @@ import Game from "./Game.js";
 console.log("starting game");
 
 let form = document.getElementById("highscore_form");
+
 form.onsubmit = function (e) {
 
     e.preventDefault();
@@ -12,8 +13,10 @@ form.onsubmit = function (e) {
     let highscore = JSON.parse(window.localStorage.getItem("highscore")) || [];
     highscore.push({"name": e.target[1].value, "points": game.score, "timestamp": date.getTime()})
     window.localStorage.setItem('highscore', JSON.stringify(highscore));
-    return false;
 
+    window.location.replace("main.html");
+
+    return true;
 }
 
 let walls = JSON.parse(window.localStorage.getItem("walls"));
