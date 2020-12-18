7b7876691bbf9494e9e3990cb7c5f56459687363
jasper <jasper.bartscher@gmail.com>
2020-12-18 14:51:19 +0100

    fix spawning of points

--- SnakeGame/src/Point/Point.js
+++ SnakeGame/src/Point/Point.js
@@ -8,8 +8,12 @@ import SpriteAnimation from "../SpriteAnimation.js";
  */
 export default class Point extends StaticGameObject {
 
+    // needed for spawning logic
+    static SIZE = 32;
+
     constructor(x, y, w, h) {
         super(x, y, w, h);
+        alert("spawned at: (x " + x + " y " + y + ")");
         this.animation = new SpriteAnimation("./Assets/Images/Apple_Bounce_v3_animation.png", 64, 64, 12);
 
         this.animation.ANIMATION_DELAY = 2;
